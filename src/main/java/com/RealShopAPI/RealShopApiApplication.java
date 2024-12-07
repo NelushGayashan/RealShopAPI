e147a4f86bb3fa0d08b79a4cb14e358d22127cc8
Nelush Gayashan <nelushgayashan@gmail.com>
2024-12-07 09:17:25 +0530

    Initial commit: Added RealShopAPI Spring Boot application

---
+++ src/main/java/com/RealShopAPI/RealShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.RealShopAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RealShopAPI/RealShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RealShopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
